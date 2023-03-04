cb017de73f829a3bd57591411df0e900123a7230
PoojaAdarsh <94611473+PoojaAdarsh@users.noreply.github.com>
2023-03-04 15:46:37 +0530

    Add files via upload

---
+++ Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Logo=()=>
+{
+    return (
+        <>
+        <div>
+            <img src="https://png.pngitem.com/pimgs/s/536-5369499_drunkyn-monkee-apple-music-white-apple-music-logo.png" alt="" />
+        </div>
+        </>
+    )
+}
+export default Logo
---
+++ Menu.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+const Menu=()=>{
+    return(
+        <div class="menuBlock">
+            <ul>
+                <li>
+                    <a href="j">home</a>
+
+                </li>
+                <li>
+                    <a href="j">login</a>
+                </li>
+                <li><a href="j">sign up</a>
+                </li>
+                <li> <a href="j"> about</a></li>
+            </ul>
+        </div>
+    )
+}
+export default Menu
+
+
