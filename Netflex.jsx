9b3e6bbec621a869ece1fd548f78481876e3ebc4
PoojaAdarsh <94611473+PoojaAdarsh@users.noreply.github.com>
2023-03-06 22:12:20 +0530

    Add files via upload

---
+++ Netflex.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import STYLE from "./netflix.module.css"
+const Netflex=()=>
+{
+    return(
+        <div className={STYLE.main}>
+            <img src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png" alt="" />
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="index.html">Series</a></li>
+                <li><a href="index.html">Films</a></li>
+                <li><a href="index.html">Latest</a></li>
+                <li><a href="index.html">MyList</a></li>
+            </ul>
+            <div className={StyleSheet.searchBar}><input type="text" name="" id="" placeholder="search" /></div>
+        </div>
+        
+        
+        
+        
+       
+            
+
+
+            
+           
+            
+         
+        
+        
+        
+        
+        
+        
+
+
+        
+        
+        
+        
+        
+        
+
+
+        
+        
+        
+
+
+
+    
+    )
+}
+export default Netflex;
+         
+         
+        
+        
+        
+        
+        
+        
+
+
+        
+        
+        
+    
+        
+        
+
+
+
+    
